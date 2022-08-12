353d28754a9e00f6228d3d16aeeb5cb2775ac4ab
Marcio Vinicius de Souza da Rocha <marcio.souza@mps.com.br>
2022-08-12 16:00:42 -0300

    criada classe Graph

---
+++ trabalhos_py/tde1/graph.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------
+# created by: dev.marcio.rocha@gmail.com
+# simple Graph class
+# ------------------------------------------------------------
+
+class Graph: 
+    def __init__():
+        self.data=None
+        self.adjacency_list=[]
+
