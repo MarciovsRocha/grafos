1c773de645c13a254724dc266a0485796734613e
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-11-15 19:46:43 -0300

    created main module

--- trabalhos_py/tde5/main.py
+++ trabalhos_py/tde5/main.py
@@ -3,17 +3,26 @@
 # ------------------------------------------------------------
 # created by: dev.marcio.rocha@gmail.com
 # ------------------------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+from grafo_aleatorio import Grafo
 
-from graph import Graph
 
-G = Graph()
-G.adiciona_vertice('marcio')
-G.adiciona_vertice('mariana')
-G.adiciona_vertice('anderson')
-G.adiciona_vertice('arthur')
-G.adiciona_aresta('marcio', 'mariana', 1)
-G.adiciona_aresta('marcio', 'anderson', 3)
-G.adiciona_aresta('mariana', 'arthur', 6)
-G.adiciona_aresta('arthur', 'mariana', 10)
-G.remove_vertice('mariana')
-G.imprime_lista_adjacencia()
+G = Grafo(
+    n_nodes=100,
+    directional=True
+)
+print(G)
+
+G.export_to_pajek()
+
+graus = G.get_all_degrees(as_list=True)
+for obj in G.get_highest_nodes():
+    print(obj)
+
+plt.hist(graus, edgecolor='black', alpha=.4)
+plt.plot([np.mean(graus), np.mean(graus)], [0, 1500], r'--', label=f'Grau médio = {np.mean(graus)}')
+plt.xlabel("Graus")
+plt.ylabel("Frequência")
+plt.legend()
+plt.show()
