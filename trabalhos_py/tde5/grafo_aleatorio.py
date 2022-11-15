2180cb8ca8f54dce8b69b3f9487c496abbf5f026
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-11-15 20:29:42 -0300

    * Adicionados atributos publicos para otimizacao de ciclos de máquina * Ajustado construtor * Ajustada funcoes para populacao do grafo

--- trabalhos_py/tde5/grafo_aleatorio.py
+++ trabalhos_py/tde5/grafo_aleatorio.py
@@ -35,28 +35,31 @@ class Grafo:
     __adjacency_list: dict
     # this prob value will generate nodes with more edges than others
     # and n-edges will follow a gaussian distribution
-    __connection_prob = 2/3
+    __connection_prob: float
     __max_degree: int
     __min_degree: int
+    __nodes_data: dict
+
+    # public attributes
+    size: int
+    order: int
 
     # ---------------------------------------------------------
     # constructor
-    def __init__(self , n_nodes: int = MIN_NODES , directional: bool = True, edges: int = MIN_EDGES):
+    def __init__(self , directional: bool = True, conn_prob: float = (2/3)):
         self.__directional = directional
         self.__adjacency_list = {}
-        # this will create a graph with n_nodes
-        # and normal distribution for edges
-        initial_nodes = int(n_nodes/3)
+        self.__connection_prob = conn_prob
         self.__max_degree = 0
         self.__min_degree = 0
-        self.__create_initial_graph(initial_nodes)
-        # this function will create a scale-free graph
-        self.__finish_graph(n_nodes=n_nodes-initial_nodes, k=edges)
+        self.__nodes_data = {}
+        self.size = 0
+        self.order = 0
 
     # ---------------------------------------------------------
     # custom printable object
     def __str__(self):
-        return f'O Grafo aleatorio possui {self.order()} vértices e {self.size()} arestas.'
+        return f'O Grafo aleatorio possui {self.order} vértices e {self.size} arestas.'
 
     # ---------------------------------------------------------
     # insert new node into graph with available name in Users.txt
@@ -65,28 +68,31 @@ class Grafo:
         if node_name in self.__adjacency_list:
             raise Exception(f'The node "{node_name}" already exists in graph.')
         self.__adjacency_list[node_name] = {}
+        if self.__directional:
+            self.__nodes_data[node_name] = {'outdegree': 0, 'indegree': 0}
+        else:
+            self.__nodes_data[node_name] = {'degree': 0}
+        self.order += 1
+        return self
 
     # ---------------------------------------------------------
     # return number of indegree of specified node
     def node_indegree(self, A):
         self.__valida_nodes(A)
-        indegree = 0
-        for node in self.__adjacency_list:
-            if (node != A) and (A in self.__adjacency_list[node]):
-                indegree += 1
-        return indegree
+        return self.__nodes_data[A]['indegree']
 
     # ---------------------------------------------------------
     # return number of outdegree of specified node
     def node_outdegree(self , A):
-        if A not in self.__adjacency_list:
-            raise Exception(f'Node "{A}" doesnt exist in graph.')
-        return len(self.__adjacency_list[A])
+        self.__valida_nodes(A)
+        return self.__nodes_data[A]['outdegree']
 
     # ---------------------------------------------------------
     # return number of indegree + outdegree of specified node
     def node_degree(self, A):
-        return self.node_outdegree(A) + self.node_indegree(A)
+        if self.__directional:
+            return self.__nodes_data[A]['outdegree'] + self.__nodes_data[A]['indegree']
+        return self.__nodes_data[A]['degree']
 
     # ---------------------------------------------------------
     # return dict or list of degrees
@@ -96,22 +102,9 @@ class Grafo:
         if as_list:
             return [self.node_degree(node) for node in self.__adjacency_list]
         degrees = {}
-        for node in self.__adjacency_list:
+        for node in self.__nodes_data:
             degrees[node] = self.node_degree(node)
         return degrees
-
-    # ---------------------------------------------------------
-    # compute the number of edges in graph
-    def size(self):
-        k = 0
-        for node in self.__adjacency_list:
-            k += len(self.__adjacency_list[node])
-        return k
-
-    # ---------------------------------------------------------
-    # compute the number of nodes in graph
-    def order(self):
-        return len(self.__adjacency_list)
 
     # ---------------------------------------------------------
     # insert new edge between two nodes
@@ -122,47 +115,19 @@ class Grafo:
             raise Exception(f'Nodes "{A}" and "{B}" are the same.')
         if self.__directional:
             self.__adjacency_list[A][B] = weight
+            self.__nodes_data[A]['outdegree'] += 1
+            self.__nodes_data[B]['indegree'] += 1
         else:
             self.__adjacency_list[A][B] = weight
             self.__adjacency_list[B][A] = weight
+            self.__nodes_data[A]['degree'] += 1
+            self.__nodes_data[B]['degree'] += 1
+        self.size += 1
         # adjust variables MIN MAX
         degrees_list = self.get_all_degrees(as_list=True)
         self.__max_degree = max(degrees_list)
         self.__min_degree = min(degrees_list)
         return self
-
-    # ---------------------------------------------------------
-    # creates an initial graph with 1k nodes and max 150 edges per node
-    def __create_initial_graph(self, n_nodes: int):
-        # creates initial nodes
-        while 0 < n_nodes:
-            self.new_node()
-            n_nodes -= 1
-        # create initial edges
-        for A in self.__adjacency_list:
-            for B in self.__adjacency_list:
-                if (A != B) and (random() > self.__connection_prob):
-                    self.new_edge(A, B, new_weight())
-
-    # ---------------------------------------------------------
-    # creates an initial graph with 1k nodes and max 150 edges per node
-    # n_nodes: number of nodes to be added
-    # k: number of edges for each new node
-    def __finish_graph(self , n_nodes: int, k: int = 3):
-        new_nodes = []
-        # creates initial nodes
-        while n_nodes > len(new_nodes):
-            new_nodes.append(new_name())
-            self.new_node(new_nodes[len(new_nodes)-1])
-        # create k-edges for each new_node
-        for A in new_nodes:
-            while k > self.node_degree(A):
-                for B in self.__adjacency_list:
-                    # (nodes not equal)(rand * B_degree > prob)
-                    if A != B:
-                        b_conn_prob = random()*min_max(MIN=self.__min_degree, MAX=self.__max_degree, x=self.node_degree(B))
-                        if b_conn_prob > self.__connection_prob:
-                            self.new_edge(A , B , new_weight())
 
     # ---------------------------------------------------------
     # creates an initial graph with 1k nodes and max 150 edges per node
@@ -189,6 +154,7 @@ class Grafo:
                              cited_colname="target",
                              weighted=True)
         writer.write("output.net")
+        return self
 
     # ---------------------------------------------------------
     # verify if all passed nodes exists in graph
@@ -199,3 +165,42 @@ class Grafo:
         if str == type(nodes):
             if not exists_nodes([nodes], [A for A in self.__adjacency_list]):
                 raise Exception(f'Node "{nodes}" doesnt exists in graph.')
+        return self
+
+    # ---------------------------------------------------------
+    # create graph with gaussian distribution of edges
+    def create_gaussian_distribution(self, n_nodes : int):
+        # creates initial nodes
+        while 0 < n_nodes:
+            self.new_node()
+            n_nodes -= 1
+        # create initial edges
+        for A in self.__adjacency_list:
+            for B in self.__adjacency_list:
+                if (A != B) and (random() > self.__connection_prob):
+                    self.new_edge(A , B , new_weight())
+        return self
+
+    # ---------------------------------------------------------
+    # create scale free graph
+    def create_scale_free(self, n_nodes: int, n_edges: int):
+        initial_nodes = int(n_nodes / 3)
+        n_nodes = n_nodes-initial_nodes
+        self.create_gaussian_distribution(n_nodes=initial_nodes)
+        # this function will create a scale-free graph
+        new_nodes = []
+        # creates initial nodes
+        while n_nodes > len(new_nodes):
+            new_nodes.append(new_name())
+            self.new_node(new_nodes[len(new_nodes) - 1])
+        # create k-edges for each new_node
+        for A in new_nodes:
+            while n_edges > self.node_degree(A):
+                for B in self.__adjacency_list:
+                    # (nodes not equal)(rand * B_degree > prob)
+                    if A != B:
+                        b_conn_prob = random() * min_max(MIN=self.__min_degree , MAX=self.__max_degree ,
+                                                         x=self.node_degree(B))
+                        if b_conn_prob > self.__connection_prob:
+                            self.new_edge(A , B , new_weight())
+        return self
