d149b78936f5edac5c421fd7b6ae37327082dac0
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-11-19 09:35:22 -0300

    * uncommented code * some formatting modifications

--- trabalhos_py/tde5/main.py
+++ trabalhos_py/tde5/main.py
@@ -2,9 +2,9 @@
 # created by: dev.marcio.rocha@gmail.com
 # ------------------------------------------------------------
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 from grafo_aleatorio import Grafo
-from  utils import *
+from utils import *
 
 
 G = Grafo(directional=True, verbose=True)
@@ -34,25 +34,25 @@ exemplo_slide = {
 }
 G.load_from_dict(exemplo_slide)
 G.get_dag()
-#G.create_gaussian_distribution(n_nodes=10)
-#meu_dict = load_json('dict_MOCK.json')
-#G.load_from_dict(meu_dict)
-#G.export_to_json('big_one_nodes.json')
-#G.import_from_pajek("output.net")
+# G.create_gaussian_distribution(n_nodes=10)
+# meu_dict = load_json('dict_MOCK.json')
+# G.load_from_dict(meu_dict)
+# G.export_to_json('big_one_nodes.json')
+# G.import_from_pajek("output.net")
 
 G.export_to_pajek()
 
-#print(G)
+# print(G)
 
-#graus = G.get_all_degrees(as_list=True)
-#for obj in G.get_highest_nodes():
-   #print(obj)
+graus = G.get_all_degrees(as_list=True)
+# for obj in G.get_highest_nodes():
+#   print(obj)
 
-#plt.hist(graus, edgecolor='black', alpha=.4)
-#plt.plot([np.mean(graus), np.mean(graus)], [0, 1500], r'--', label=f'Grau médio = {np.mean(graus)}')
-#plt.xlabel("Graus")
-#plt.ylabel("Frequência")
-#plt.legend()
-#plt.show()
+plt.hist(graus, edgecolor='black', alpha=.4)
+plt.plot([np.mean(graus), np.mean(graus)], [0, 1500], r'--', label=f'Grau médio = {np.mean(graus)}')
+plt.xlabel("Graus")
+plt.ylabel("Frequência")
+plt.legend()
+plt.show()
 
-#G.get_strongly_connected_components()
+# G.get_strongly_connected_components()
