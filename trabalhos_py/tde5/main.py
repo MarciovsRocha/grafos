0f08468e6d5a0cec602f1571eae09b0397fa8096
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-11-19 10:59:17 -0300

    * standardized way of feeding graph and mocked data * added mocked file names * adjusts method calls * moved histogram plot to utils.py

--- trabalhos_py/tde5/main.py
+++ trabalhos_py/tde5/main.py
@@ -6,53 +6,37 @@ import matplotlib.pyplot as plt
 from grafo_aleatorio import Grafo
 from utils import *
 
+# ------------------------------------------------------
+# some mocked data
+SIMPLE_MOCK = 'dict_MOCK.json'
+EXEMPLOS_SLIDE = 'exemplos_slide.json'
+FIVE_HUNDRED_NODES = 'exported_graph_500_nodes.json'
+BIG_ONE = 'big_one_nodes.json'
+
+
+def feed_graph(graph: Grafo, option: str, n_nodes: int = 500, n_edges: int = 3):
+    my_dict = {}
+    if 'gaussian' == option.lower():
+        graph.create_gaussian_distribution(n_nodes=n_nodes)
+        return
+    elif 'scale_free' == option.lower():
+        graph.create_scale_free(n_nodes=n_nodes, n_edges=n_edges)
+        return
+    my_dict = load_json(option)
+    graph.load_from_dict(my_dict)
+
 
 G = Grafo(directional=True, verbose=True)
-#G.create_scale_free(n_nodes=100, n_edges=3)
-exemplo_slide = {
-    'H': {
-        'G': 1,
-        'B': 1
-    },
-    'A': {
-        'B': 1,
-        'F': 1
-    },
-    'B': {
-        'G': 1,
-        'F': 1
-    },
-    'F': {
-        'C': 1,
-        'I': 1
-    },
-    'C': {},
-    'I': {},
-    'G': {
-        'I': 1
-    }
-}
-G.load_from_dict(exemplo_slide)
-G.get_dag()
-# G.create_gaussian_distribution(n_nodes=10)
-# meu_dict = load_json('dict_MOCK.json')
-# G.load_from_dict(meu_dict)
-# G.export_to_json('big_one_nodes.json')
+feed_graph(
+    graph=G
+    , option=FIVE_HUNDRED_NODES
+#    , n_nodes=500
+#    , n_edges=None
+)
+# G.get_dag()
+
+# G.export_to_json(FIVE_HUNDRED_NODES)
 # G.import_from_pajek("output.net")
-
-G.export_to_pajek()
-
-# print(G)
-
-graus = G.get_all_degrees(as_list=True)
-# for obj in G.get_highest_nodes():
-#   print(obj)
-
-plt.hist(graus, edgecolor='black', alpha=.4)
-plt.plot([np.mean(graus), np.mean(graus)], [0, 1500], r'--', label=f'Grau médio = {np.mean(graus)}')
-plt.xlabel("Graus")
-plt.ylabel("Frequência")
-plt.legend()
-plt.show()
-
+# G.export_to_pajek()
+G.degrees_histogram()
 # G.get_strongly_connected_components()
